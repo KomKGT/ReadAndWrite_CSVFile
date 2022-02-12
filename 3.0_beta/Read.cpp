903e384db73deff6bfc766635fa6cb775dab7844
KomKGT <81642936+KomKGT@users.noreply.github.com>
2022-02-12 12:47:04 +0700

    Add files via upload

---
+++ 3.0_beta/Read.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+double D_convertstringTodoubleOfx();
+double D_convertstringTodoubleOfy();
+void V_readCSVfile();
+
+int I_count = 0;
+string str_x[100];
+string str_y[100];
+
+double D_x[100];
+vector<double> D_xx;
+double D_y[100];
+vector<double> D_yy;
+
+double x;
+double y;
+
+int i = 0;
+
+double D_convertstringTodoubleOfx()
+{
+	if(I_count>=1)
+	{
+		stringstream convertx(str_x[I_count]);
+		convertx >> D_x[I_count];
+        return D_x[I_count];
+	}
+	
+}
+double D_convertstringTodoubleOfy()
+{
+	if(I_count>=1)
+	{
+		stringstream converty(str_y[I_count]);
+		converty >> D_y[I_count];
+        return D_y[I_count];
+	}
+
+}
+void V_readCSVfile()
+{
+	ifstream MyReadFile("xy.csv"); //Re file position
+	
+	if(!MyReadFile.is_open())
+		cout<<"File Can't Open"<<endl;
+		
+	string line;
+	
+	while(getline(MyReadFile,line))
+	{
+		stringstream ss(line);
+		
+		getline(ss,str_x[I_count],',');
+		getline(ss,str_y[I_count],'\n');
+		
+		if(I_count >= 1)
+		{
+			//D_x[I_count] = D_convertstringTodoubleOfx();
+			//D_y[I_count] = D_convertstringTodoubleOfy();
+			D_xx.push_back(D_convertstringTodoubleOfx());
+			D_yy.push_back(D_convertstringTodoubleOfy());	
+		}
+		
+		I_count++;
+		
+		//if(I_count ==100 )
+		//	I_count == 0;
+			
+		
+	}
+}
+
+int main(int argc, char** argv) {
+	 
+	V_readCSVfile();
+	
+	
+	
+	for(int i =1;i<=D_xx.size();i++)
+	{
+		cout<<"x = "<<D_x[i]<<" "<<"y = "<<D_y[i]<<"\n";
+	}
+	//cout<<"x = "<<x<<" "<<"y = "<<y<<"\n";
+	//V_readCSVfile();
+	//cout<<"x = "<<D_x[2]<<" "<<"y = "<<D_y[2]<<"\n";
+	
+	return 0;
+}
+
